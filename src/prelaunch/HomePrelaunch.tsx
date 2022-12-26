a28526c0bac34362f858596e37e7ef31368d317b
Alizeh Khan <alizehedinburgh@gmail.com>
2022-12-26 22:22:15 +0100

    fix: form css

--- src/prelaunch/HomePrelaunch.tsx
+++ src/prelaunch/HomePrelaunch.tsx
@@ -1,13 +1,15 @@
-import { Heading, Text } from "../design-system/components"
+import { IconArrowNarrowDown, IconSend } from "@tabler/icons"
+
+import { Box, Heading, Text } from "../design-system/components"
 import Features from "./Features"
 import Footer from "./Footer"
 import "./stylesheets/style.css"
 
 export default () => (
 	<>
-		<header className="bg-hero">
-			<div className="container">
-				<img src="/images/logo/logo.svg" className="center-logo" />
+		<Box as="header" className="bg-hero">
+			<Box className="container">
+				<Box as="img" src="/images/logo/logo.svg" className="center-logo" />
 				<Heading as="h1" className="tagline">
 					Plan your trail accommodation
 				</Heading>
@@ -17,7 +19,6 @@ export default () => (
 				</Text>
 				<form
 					id="form-request-access"
-					className="hide-submit"
 					// method="POST"
 					// action="/"
 				>
@@ -34,10 +35,9 @@ export default () => (
           document.querySelector('#id_email').classNames =
             "{% if inputMessage in 'error,userExists' %}shake{% endif %}"
         </script> */}
-					<label className="btn-send">
-						<input type="submit" />
-						<i className="ti ti-send"></i>
-					</label>
+					<button type="submit" className="btn-send">
+						<IconSend color="white" size={24} />
+					</button>
 				</form>
 				{/* {% if inputMessage == "success" %}
         <p className="input-message">
@@ -48,81 +48,81 @@ export default () => (
       {% elif inputMessage == "error" %}
         <p className="input-message">⚠️ Oops something went wrong, please try again!</p>
       {% endif %} */}
-				<i className="ti ti-arrow-narrow-down"></i>
-			</div>
-		</header>
+				<IconArrowNarrowDown size={40} className="ti-arrow-narrow-down" />
+			</Box>
+		</Box>
 
-		<main>
+		<Box as="main">
 			{/* STEP 1 */}
-			<section className="bg-gray">
-				<div className="container wrapper-step">
-					<div className="text">
+			<Box as="section" className="bg-gray">
+				<Box className="container wrapper-step">
+					<Box className="text">
 						<Text fontFamily="font-family-serif" className="number">
 							1
 						</Text>
-						<div>
+						<Box>
 							<Heading>Explore your trail</Heading>
 							<Text fontSize="font-size-4">
 								Explore mountain huts and campsites along on your trail. See the
 								trail's stages, water sources, and resupply points.
 							</Text>
 							<button className="request-access">Request access</button>
-						</div>
-					</div>
-					<div className="step-1-image">
+						</Box>
+					</Box>
+					<Box className="step-1-image">
 						<img
 							src="/images/step/1.svg"
 							alt="Preview of adding an accommodation to your itinerary and checking availability"
 						/>
-					</div>
-				</div>
-			</section>
+					</Box>
+				</Box>
+			</Box>
 			{/* STEP 2 */}
-			<section className="bg-white">
-				<div className="container wrapper-step">
+			<Box as="section" className="bg-white">
+				<Box className="container wrapper-step">
 					<img
 						src="/images/step/2.svg"
 						alt="Preview of adding an accommodation to your itinerary and checking availability"
 					/>
-					<div className="text">
+					<Box className="text">
 						<Text fontFamily="font-family-serif" className="number">
 							2
 						</Text>
-						<div>
+						<Box>
 							<Heading>Build your itinerary</Heading>
 							<Text fontSize="font-size-4">
 								Check availability and add the campsite or hut you'd like to
 								stay at to your itinerary. See your trip unfold on the map.
 							</Text>
 							<button className="request-access">Request access</button>
-						</div>
-					</div>
-				</div>
-			</section>
+						</Box>
+					</Box>
+				</Box>
+			</Box>
 			{/* STEP 3 */}
-			<section className="bg-gray">
-				<div className="container wrapper-step">
-					<div className="text">
+			<Box as="section" className="bg-gray">
+				<Box className="container wrapper-step">
+					<Box className="text">
 						<Text fontFamily="font-family-serif" className="number">
 							3
 						</Text>
-						<div>
+						<Box>
 							<Heading>Confirm and book</Heading>
 							<Text fontSize="font-size-4">
 								Book and pay for all the accommodation in your itinerary in one
 								go without leaving the site.
 							</Text>
 							<button className="request-access">Request access</button>
-						</div>
-					</div>
+						</Box>
+					</Box>
 					<img
 						src="/images/step/3.svg"
 						alt="Preview of adding an accommodation to your itinerary and checking availability"
 					/>
-				</div>
-			</section>
+				</Box>
+			</Box>
 			<Features />
-		</main>
+		</Box>
 		<Footer />
 	</>
 )
